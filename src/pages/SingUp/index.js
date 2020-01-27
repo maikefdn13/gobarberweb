325d250cbb70c3f305f3d95f110cfc5e53f5020f
Jose Maike Rodrigues de Sousa <maikerodrigues@cialne.com.br>
2020-01-27 16:51:32 -0300

    Ultilizando UnForm

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SingUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input placeholder="Nome completo" />
+        <Input type="email" placeholder="Seu e-mail" />
+        <Input type="password" placeholder="Sua senha secreta" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
